927c490dc0c75b79cbc8de65b5f8f4c14ce41b45
Lucas Walger do Nascimento <walger.profissional@gmail.com>
2022-01-21 19:07:59 -0300

    renamed isInBounderies method to IsInBounderies method

--- codigo/Camp.java
+++ codigo/Camp.java
@@ -1,8 +1,9 @@
+
 /*Camp of the Tetris game */
 public class Camp {
     private int height, width; //height and width of the camp
     private char[][] camp; //camp matrix, where 0 means empty, and other numbers mean occupied. Each number has a color associated with it
-    
+
     public Camp(int height, int width) {
         this.height = height;
         this.width = width;
@@ -21,7 +22,7 @@ public class Camp {
     public boolean IsOccupied(int x, int y)
     {
         //tests if it is on the bounderies of the camp.
-        if (!isInBounderies(x, y))
+        if (!IsInBounderies(x, y))
         {
             System.out.printf("\nError, x: %d and y: %d are out of bounderies of Camp\n",x,y);
             System.exit(1);
@@ -29,8 +30,9 @@ public class Camp {
         }
         return ( camp[y][x] == 0 ? false : true);
     }
+    
     //returns true if location is inside of Camp
-    public boolean isInBounderies(int x, int y)
+    public boolean IsInBounderies(int x, int y)
     {
         if(x<0 ||x>= width || y<0 || y>= height)
             return false;
